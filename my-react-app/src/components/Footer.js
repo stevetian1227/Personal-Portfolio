9b03fff540300e81e802773d81f3c6869cd701ba
Tianqing Gao <48400186+stevetian1227@users.noreply.github.com>
2023-06-29 09:17:07 +0930

    minor update on the style of the foter
    update the footer css to make the footer look nicer and add one more icon to the footer

--- my-react-app/src/components/Footer.js
+++ my-react-app/src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from "react";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
 import "../styles/Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
-      <div>
+      <div className="socialMedia">
         <LinkedInIcon />
+        <GitHubIcon />
       </div>
       <p> Mobile: 0413938895 </p>
       <p> Email: stevegao1227@gmail.com </p>
