d40ef2d2f8d4cd9c409fc30132761c3109634095
Tianqing Gao <48400186+stevetian1227@users.noreply.github.com>
2023-06-29 22:42:08 +0930

    Update on the Home Page
    Add the style and the content of the Home page

--- my-react-app/src/pages/Home.js
+++ my-react-app/src/pages/Home.js
@@ -1,7 +1,49 @@
 import React from "react";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GithubIcon from "@material-ui/icons/GitHub";
+import EmailIcon from "@material-ui/icons/Email";
+import "../styles/Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="about">
+        <h2>Hi, my name is Steve</h2>
+        <div className="prompt">
+          <p>
+            A software developer with a passion for learning, developing and
+            creating.
+          </p>
+          <LinkedInIcon />
+          <GithubIcon />
+          <EmailIcon />
+        </div>
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2>Front-end</h2>
+            <span>
+              React, HTML, CSS, Bootstrap, Flutter, MaterialUI, TailwindCSS
+            </span>
+          </li>
+          <li className="item">
+            <h2>Back-end</h2>
+            <span>NodeJS, .Net, ExpressJS, MySQL, MangoDB, MSSQL</span>
+          </li>
+          <li className="item">
+            <h2>Language</h2>
+            <span>Python, Java, C, C++, C#, R, JavaScript</span>
+          </li>
+          <li className="item">
+            <h2>Cloud</h2>
+            <span>AWS</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
