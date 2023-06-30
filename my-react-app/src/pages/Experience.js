504c2b282e7884b569218b19b42d43bbf4b42f17
Tianqing Gao <48400186+stevetian1227@users.noreply.github.com>
2023-06-30 16:04:36 +0930

    update the experience component
    Add the information needed for the experience components

--- my-react-app/src/pages/Experience.js
+++ my-react-app/src/pages/Experience.js
@@ -1,7 +1,36 @@
 import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@material-ui/icons/School";
 
 function Experience() {
-  return <div>Experience</div>;
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element-education"
+          date="2016-2019"
+          icon={<SchoolIcon />}
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+        >
+          <h3>Monash University, Victoria, Australia</h3>
+          <p>Bahelor of Data Science</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element-education"
+          date="2020-2022"
+          icon={<SchoolIcon />}
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+        >
+          <h3>University of Adelaide, South Australia, Australia</h3>
+          <p>Master of Software Engineering</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
 export default Experience;
