2573bbffa440748289ebf746a11a32c03a81dc89
Tianqing Gao <48400186+stevetian1227@users.noreply.github.com>
2023-07-18 12:58:44 +0930

    fix some bugs in the projects component and create the component for Project to be displayed

---
+++ my-react-app/src/helpers/ProjectList.js
@@ -0,0 +1,16 @@
+import Proj1 from "../assets/proj2.jpg";
+
+export const projectList = [
+  {
+    name: "Project 1",
+    image: Proj1,
+  },
+  {
+    name: "Project 2",
+    image: Proj1,
+  },
+  {
+    name: "Project 3",
+    image: Proj1,
+  },
+];
--- my-react-app/src/pages/Projects.js
+++ my-react-app/src/pages/Projects.js
@@ -1,7 +1,19 @@
 import React from "react";
+import ProjectItem from "../components/ProjectItem";
+import "../styles/Projects.css";
+import { projectList } from "../helpers/ProjectList";
 
 function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="projects">
+      <h1>My Projects</h1>
+      <div className="projectList">
+        {projectList.map((project) => {
+          return <ProjectItem name={project.name} image={project.image} />;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
